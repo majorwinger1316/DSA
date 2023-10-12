7759e55120658733edc1e7969af0fe4dbddd89d5
majorwinger1316 <144731308+majorwinger1316@users.noreply.github.com>
2023-10-12 14:56:53 +0530

    Create infixtopostfix.c

---
+++ Q-9/infixtopostfix/infixtopostfix.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "infixtopostfix.h"
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    if(top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+
+int priority(char x)
+{
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+    return 0;
+}
